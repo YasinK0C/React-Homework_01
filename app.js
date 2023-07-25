de63f02786ac1050fdf0f9b4bf1ed028d9ef7e3c
Yasin K0C <kocyasin@gmail.com>
2023-07-25 15:11:32 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+ //Getting axios package from axios.
+
+ async function getData(id) {
+
+    //Getting first request
+    
+    const { data: firstRequest } = await axios(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+
+    //Getting second request
+    
+    const { data: secondRequest } = await axios(
+      `https://jsonplaceholder.typicode.com/posts?userId=${id}`
+    );
+  
+  // Two request in a same Object that was required
+    
+  const newObj = { firstRequest, Posts: secondRequest };
+  console.log(newObj);
+}
+
+// I am going to export here because i need to import in index.js
+
+export default getData;
---
+++ index.js
@@ -0,0 +1,7 @@
+//Importing getData function from app.js 
+
+import getData from "./app.js";
+
+//Running get data function.
+
+console.log(getData(1));
